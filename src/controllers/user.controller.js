0ecfd7268b8d97a4ed8555711ed5381c61bcf1ef
prathyu116 <prathyuprasad116@gmail.com>
2024-06-21 16:25:09 +0530

    removed password

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,12 +9,35 @@ const router = express.Router();
 
 router.get("/all-users", authenticate,async (req, res) => {
     try {
-        const users = await User.find();
+        const users = await User.find().select('-password');
         res.send(users);
     } catch (error) {
         res.status(400).send(error);
     }
 
 })
+router.get("/single-user/:id", authenticate,async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).select('-password');
+        if (!user) {
+            return res.status(404).send({ message: 'User not found' });
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+})
+// exports.getUser = async (req, res) => {
+//     try {
+//         const user = await User.findById(req.params.id).select('-password');
+//         if (!user) {
+//             return res.status(404).send({ message: 'User not found' });
+//         }
+//         res.send(user);
+//     } catch (error) {
+//         res.status(400).send(error);
+//     }
+// };
 
 module.exports = router;
